491dd4558e6670265f980bf401adf24cb2f5a8e3
LeoCodeRu <leo@leocode.ru>
2024-09-06 17:56:11 +0300

    Rebase git

--- examples/another/test_eeprom/Debug/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Leo\AppData\Local\Temp\VMBuilds\test_eeprom\mega_atmega2560\Debug\Wire\utility\twi.c.o: \
- C:\Users\Leo\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\libraries\Wire\src\utility\twi.c \
- C:\Users\Leo\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
- C:\Users\Leo\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/binary.h \
- C:\Users\Leo\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\variants\mega/pins_arduino.h \
- C:\Users\Leo\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\libraries\Wire\src\utility\twi.h
